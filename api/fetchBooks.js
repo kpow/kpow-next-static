0518d428df555d69afcffba8625fc28575679a6e
Kevin Power <kpow@kpow.com>
2020-10-19 19:16:48 -0400

    protocol tweak

--- api/fetchBooks.js
+++ api/fetchBooks.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import convert from 'xml-js'
 
 const totalBooks = async () =>{
-  const { data } = await axios.get("http://services.kpow.com/total_books.php")
+  const { data } = await axios.get("https://services.kpow.com/total_books.php")
   const shelfJson = JSON.parse(convert.xml2json(data, { compact: true, spaces: 4 }))
   const shelves = shelfJson.GoodreadsResponse.shelves.user_shelf
   const totals = shelves.map((shelf)=> Number(shelf.book_count._text) )
