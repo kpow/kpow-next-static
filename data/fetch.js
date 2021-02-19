e8787967ce984c2c9b5a565ddebec06e8ccd5996
Kevin Power <kpow@kpow.com>
2021-02-18 22:59:24 -0500

    added env secret

--- data/fetch.js
+++ data/fetch.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
-
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const { LAST_FM_KEY } = process.env;
+const API_ENDPOINT = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=krakap&api_key="+LAST_FM_KEY+"&format=json&limit=50page=1";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
-      body: data.joke,
+      body: data,
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
