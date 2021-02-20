a5c141bd9bbc9c3ee5c8d9a4d503467cf2f2e601
Kevin Power <kpow@kpow.com>
2021-02-20 12:12:07 -0500

    2nd swing

--- api/fetchScrobbles.js
+++ api/fetchScrobbles.js
@@ -4,12 +4,12 @@ const fetchScrobbles = async (key, page = 0, howMany = 9) => {
   
   const { data } = await axios.get('https://kpow-wow.com/.netlify/functions/fetch')
 
-  const totalItems = data.recenttracks.track.length;
-  const totalPages = data.recenttracks.track.length/howMany
+  const totalItems = data.data.recenttracks.track.length;
+  const totalPages = data.data.recenttracks.track.length/howMany
 
   const hasMore = true;
   
-  const fullData = {data:data.recenttracks.track,totalItems,hasMore}
+  const fullData = {data:data.data.recenttracks.track,totalItems,hasMore}
   console.log(fullData)
   return fullData
 }
