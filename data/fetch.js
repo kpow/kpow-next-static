69e24eec9a0847eb193c38e3efc20044968904b4
Kevin Power <kpow@kpow.com>
2021-02-19 22:50:06 -0500

    6

--- data/fetch.js
+++ data/fetch.js
@@ -1,26 +1,15 @@
+const fetch = require("node-fetch");
 
-const fetch = require('node-fetch')
-
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
 exports.handler = async (event, context) => {
-  let response
-  try {
-    response = await fetch(API_ENDPOINT)
-    // handle response
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
       body: JSON.stringify({
-        error: err.message
-      })
-    }
-  }
-  console.log(response)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: response
-    })
-  }
-}
+        data: response
+      }),
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
+};
