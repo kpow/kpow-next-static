7b85e716362c5c8f567bf3976466b2c55e592449
Kevin Power <kpow@kpow.com>
2021-02-20 12:24:00 -0500

    got it local dev and prod of serverless functions

--- api/fetchScrobbles.js
+++ api/fetchScrobbles.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const fetchScrobbles = async (key, page = 0, howMany = 9) => {
   
-  const { data } = await axios.get('https://kpow-wow.com/.netlify/functions/fetch')
+  const { data } = await axios.get('.netlify/functions/fetch')
 
   const totalItems = data.data.recenttracks.track.length;
   const totalPages = data.data.recenttracks.track.length/howMany
