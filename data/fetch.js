551b24b2695b37371fee54ebfff12dc63f58df20
Kevin Power <kpow@kpow.com>
2021-02-20 00:43:52 -0500

    9

--- data/fetch.js
+++ data/fetch.js
@@ -1,6 +1,9 @@
 const fetch = require("node-fetch");
+const { LAST_FM_KEY } = process.env;
+const API_ENDPOINT = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=krakap&api_key="+LAST_FM_KEY+"&format=json&limit=50page=1";
 
-const API_ENDPOINT = "https://services.kpow.com/scrobbles.php?page="+1+"&perPage="+10;
+
+// const API_ENDPOINT = "https://services.kpow.com/scrobbles.php?page="+1+"&perPage="+109;
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
