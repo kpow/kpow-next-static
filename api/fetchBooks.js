6be91a6aa7277ace27c7d45dbba4609992b0d54c
Kevin Power <kpow@kpow.com>
2021-02-20 22:41:01 -0500

    moving books to lambda

--- api/fetchBooks.js
+++ api/fetchBooks.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import convert from 'xml-js'
 
 const totalBooks = async () =>{
-  const { data } = await axios.get("https://services.kpow.com/total_books.php")
+  const { data } = await axios.post("/.netlify/functions/totalBooks")
   const shelfJson = JSON.parse(convert.xml2json(data, { compact: true, spaces: 4 }))
   const shelves = shelfJson.GoodreadsResponse.shelves.user_shelf
   const totals = shelves.map((shelf)=> Number(shelf.book_count._text) )
@@ -11,11 +11,11 @@ const totalBooks = async () =>{
 }
 
 const fetchBooks = async (key, page = 0, howMany = 4) => {
-  const { data } = await axios.get("https://services.kpow.com/books.php?perPage="+howMany+"&page="+(Number(page)+1))
+  const { data } = await axios.post('/.netlify/functions/books',{page:page, howMany:howMany})
   const json = JSON.parse(convert.xml2json(data, { compact: true, spaces: 4 }))
   const bookData = json.GoodreadsResponse.reviews.review
-  const totalItems = await totalBooks();
-  //const totalItems = 497;
+  //const totalItems = await totalBooks();
+  const totalItems = 497;
   const fullData = {data:bookData, totalItems, hasMore:true}
   return fullData
 }
