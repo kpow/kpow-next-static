55315a075c57dd76d9597b09c3018027cfebb656
Kevin Power <kpow@kpow.com>
2021-02-25 00:12:36 -0500

    updated react, updated next, added leaflet

---
+++ pages/donuts.js
@@ -0,0 +1,40 @@
+import Layout from '@components/Layout'
+import { Container, Grid, Button, Paper } from '@material-ui/core';
+import Box from '@material-ui/core/Box'
+import Link from 'next/link';
+import NavigateNextIcon from '@material-ui/icons/NavigateNextOutlined'
+import Divider from '@material-ui/core/Divider'
+import Title from '@components/shared/Title';
+import dynamic from "next/dynamic";
+
+const Donuts = ({ title, description, ...props }) => {
+
+  const MapWithNoSSR = dynamic(() => import("../components/Map"), {
+    ssr: false
+  });
+
+  return (
+    <Layout pageTitle={`${title} | About`} description={description}>
+      <Container maxWidth="md">
+
+        <MapWithNoSSR />
+
+      </Container>
+    </Layout> 
+  );
+}
+
+export default Donuts
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  }
+}
+
+
