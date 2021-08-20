86db993f6011dad6cffcfcb7fcbf0cd23ae0564a
Kevin Power <kpow@kpow.com>
2021-08-20 16:14:42 -0400

    maybe the last of the lintz

--- components/music/TopSongBox.js
+++ components/music/TopSongBox.js
@@ -1,20 +1,24 @@
-import {useEffect, useState} from 'react';
-import jsonp from 'jsonp';
-import Computation from "@utils/musicComputation";
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import Computation from '@utils/musicComputation';
 
-const TopSongBox = ({song}) => {
-    const topSong = song;
+const TopSongBox = ({ song }) => {
+  const topSong = song;
 
-    return (
-        <div>
-            <h3>Your most played song on Apple Music is</h3>
-            <h1>{topSong.key}</h1>
-            <p>
-                You've played this <strong>{topSong.value.plays}</strong> times for a total of 
-                <strong>{Computation.convertTime(topSong.value.time)}</strong>
-            </p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h3>Your most played song on Apple Music is</h3>
+      <h1>{topSong.key}</h1>
+      <p>
+        You've played this
+        {' '}
+        <strong>{topSong.value.plays}</strong>
+        {' '}
+        times for a total of
+        <strong>{Computation.convertTime(topSong.value.time)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default TopSongBox;
--- components/music/TotalsBoxes.js
+++ components/music/TotalsBoxes.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
-import Computation from "@utils/musicComputation";
+import React from 'react';
+import Computation from '@utils/musicComputation';
 import numeral from 'numeral';
 import Grid from '@material-ui/core/Grid';
 
 const TotalsBoxes = (props) => {
-    const totals = props.totals;
-    const songs= props.songs;
-    const artists = props.artists;
+  const { totals } = props;
+  const { songs } = props;
+  const { artists } = props;
 
-    return (
-        <Grid container spacing={3} key="totals">
-            <Grid container item xs={12} sm={10} md={6} style={{margin:'0 auto'}}>
-                <p className="lead">time: </p>
-                <h2>{Computation.convertTime(totals.totalTime)}</h2>
-                
-            </Grid>
-            <Grid container item xs={12} sm={10} md={2} style={{margin:'0 auto'}}>
-                <p className="lead">plays: </p>
-                <h2>{numeral(totals.totalPlays).format('0,0')}</h2>
-                
-            </Grid>
-            <Grid container item xs={12} sm={10} md={2} style={{margin:'0 auto'}}>
-                <p className="lead">songs: </p>
-                <h2>{numeral(songs).format('0,0')}</h2>
-                
-            </Grid>
-            <Grid container item xs={12} sm={10} md={2} style={{margin:'0 auto'}}>
-                <p className="lead">artists: </p>
-                <h2>{numeral(artists).format('0,0')}</h2>
-                
-            </Grid>
-        </Grid>
-    );
-}
+  return (
+    <Grid container spacing={3} key="totals">
+      <Grid container item xs={12} sm={10} md={6} style={{ margin: '0 auto' }}>
+        <p className="lead">time: </p>
+        <h2>{Computation.convertTime(totals.totalTime)}</h2>
+
+      </Grid>
+      <Grid container item xs={12} sm={10} md={2} style={{ margin: '0 auto' }}>
+        <p className="lead">plays: </p>
+        <h2>{numeral(totals.totalPlays).format('0,0')}</h2>
+
+      </Grid>
+      <Grid container item xs={12} sm={10} md={2} style={{ margin: '0 auto' }}>
+        <p className="lead">songs: </p>
+        <h2>{numeral(songs).format('0,0')}</h2>
+
+      </Grid>
+      <Grid container item xs={12} sm={10} md={2} style={{ margin: '0 auto' }}>
+        <p className="lead">artists: </p>
+        <h2>{numeral(artists).format('0,0')}</h2>
+
+      </Grid>
+    </Grid>
+  );
+};
 
 export default TotalsBoxes;
